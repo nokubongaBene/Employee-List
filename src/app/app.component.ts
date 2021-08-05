fe6cd6d57d51ef5d6638dfa08f07d3cdfee14aa8
nokubongäBene <benedictankambs@gmail.com>
2021-08-05 21:22:35 +0200

    update css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'employee-list-activity';
-  pageStatus = true;
+  pageStatus = false;
   Firstname="";
   userPassword="";
 
